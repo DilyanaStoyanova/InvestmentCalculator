f5497c21de36ac0884a429593800e58a5f04742e
DilyanaStoyanova <dilyanazhstoyanova@gmail.com>
2023-11-09 14:42:11 +0200

    Handle events two-way binding

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,8 +1,12 @@
 import Header from "./components/Header";
+import UserInput from "./components/Userinput";
 
 function App() {
 	return (
-		<Header />
+		<>
+			<Header />
+			<UserInput />
+		</>
 	);
 }
 
